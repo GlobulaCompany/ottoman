5fb9c31d24b7969509474f83d60c9ecf894d9c0e
anida15 <anidakimtai@gmail.com>
2024-05-02 09:47:13 +0300

    updates

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,8 +2,11 @@ import React, { useState, useEffect, useRef } from 'react';
 import { MdOutlineDashboard } from 'react-icons/md';
 import { AiOutlineUser, AiOutlineTeam, AiOutlineHeart } from 'react-icons/ai';
 import { FiMessageSquare, FiFolder } from 'react-icons/fi';
+import { RiSettings4Line } from 'react-icons/ri';
+import { TbReportAnalytics } from 'react-icons/tb';
 import { HiMenuAlt3 } from 'react-icons/hi';
 import { AiOutlineClose } from 'react-icons/ai';
+
 
 const Sidebar = () => {
   const [open, setOpen] = useState(false);
@@ -72,6 +75,12 @@ const Sidebar = () => {
           <a href="/files" className="flex mt-2 pb-1 hover:bg-gray-900 focus:bg-dark-900 rounded-lg transition duration-300 ease-in-out">
             <FiFolder size={20} className="mt-2 mr-2 text-yellow-500" />
             <span className={`mt-2 ${open ? 'block' : 'hidden'} transition-opacity duration-300 ease-in-out opacity-${open ? '100' : '0'} ml-${open ? '2' : '0'}`}>Files</span>
+          </a>
+        </li>
+        <li className="mb-5">
+          <a href="/settings" className="flex mt-2 pb-1 hover:bg-gray-900 focus:bg-dark-900 rounded-lg transition duration-300 ease-in-out">
+            <RiSettings4Line size={20} className="mt-2 mr-2 text-yellow-500" />
+            <span className={`mt-2 ${open ? 'block' : 'hidden'} transition-opacity duration-300 ease-in-out opacity-${open ? '100' : '0'} ml-${open ? '2' : '0'}`}>Settings</span>
           </a>
         </li>
         {/* Add more manual entries for other modules */}
