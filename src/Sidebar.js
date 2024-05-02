84c6c9a35502901dd00b8f1a442bdf7cc1329046
anida15 <anidakimtai@gmail.com>
2024-05-02 10:48:12 +0300

    updates

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,6 +5,8 @@ import { FiMessageSquare, FiFolder } from 'react-icons/fi';
 import { HiMenuAlt3 } from 'react-icons/hi';
 import { AiOutlineClose } from 'react-icons/ai';
 import { RiSettings4Line } from 'react-icons/ri';
+import { AiOutlineInfoCircle } from 'react-icons/ai';
+
 
 
 const Sidebar = () => {
@@ -80,6 +82,12 @@ const Sidebar = () => {
           <a href="/faqs" className="flex mt-2 pb-1 hover:bg-gray-900 focus:bg-dark-900 rounded-lg transition duration-300 ease-in-out">
             <RiSettings4Line size={20} className="mt-2 mr-2 text-yellow-500" />
             <span className={`mt-2 ${open ? 'block' : 'hidden'} transition-opacity duration-300 ease-in-out opacity-${open ? '100' : '0'} ml-${open ? '2' : '0'}`}>FAQs</span>
+          </a>
+        </li>
+        <li className="mb-5">
+          <a href="/about" className="flex mt-2 pb-1 hover:bg-gray-900 focus:bg-dark-900 rounded-lg transition duration-300 ease-in-out">
+            <AiOutlineInfoCircle size={20} className="mt-2 mr-2 text-yellow-500" />
+            <span className={`mt-2 ${open ? 'block' : 'hidden'} transition-opacity duration-300 ease-in-out opacity-${open ? '100' : '0'} ml-${open ? '2' : '0'}`}>About Us</span>
           </a>
         </li>
         
