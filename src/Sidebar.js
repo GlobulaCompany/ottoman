e87999ad6094d71177f63c46be4a80056be92a57
anida15 <anidakimtai@gmail.com>
2024-05-02 09:40:46 +0300

    updates

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -68,6 +68,12 @@ const Sidebar = () => {
             <span className={`mt-2 ${open ? 'block' : 'hidden'} transition-opacity duration-300 ease-in-out opacity-${open ? '100' : '0'} ml-${open ? '2' : '0'}`}>Messages</span>
           </a>
         </li>
+        <li className="mb-5">
+          <a href="/files" className="flex mt-2 pb-1 hover:bg-gray-900 focus:bg-dark-900 rounded-lg transition duration-300 ease-in-out">
+            <FiFolder size={20} className="mt-2 mr-2 text-yellow-500" />
+            <span className={`mt-2 ${open ? 'block' : 'hidden'} transition-opacity duration-300 ease-in-out opacity-${open ? '100' : '0'} ml-${open ? '2' : '0'}`}>Files</span>
+          </a>
+        </li>
         {/* Add more manual entries for other modules */}
       </ul>
     </div>
