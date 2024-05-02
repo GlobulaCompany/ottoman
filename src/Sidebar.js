28903c6475cd6a419c4799b9196da4daa57ed136
anida15 <anidakimtai@gmail.com>
2024-05-02 09:14:02 +0300

    i

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -31,36 +31,29 @@ const Sidebar = () => {
     setOpen(!open);
   };
 
-  const modules = [
-    { name: "Dashboard", link: '/', icon: MdOutlineDashboard },
-    { name: "Services", link: '/services', icon: AiOutlineUser },
-    { name: "About Us", link: '/about', icon: FiMessageSquare },
-    { name: "Testimonials", link: '/testimonials', icon: FiFolder },
-    { name: "Meet Our Team", link: '/team', icon: AiOutlineTeam },
-    { name: "Blog/News", link: '/blog', icon: AiOutlineHeart },
-    { name: "FAQs", link: '/faqs', icon: RiSettings4Line },
-    { name: "Security Measures", link: '/security', icon: TbReportAnalytics },
-  ];
-
   return (
     <div ref={sidebarRef} className='text-white' style={{ backgroundColor: '#1a365d', position: 'fixed', top: '0', left: '0', zIndex: '100', width: open ? '200px' : '40px', height: '100%', boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)', transition: 'width 0.3s ease', paddingLeft: '10px' }}>
       <div className='pb-1 pt-3'>
         <AiOutlineClose style={{ display: open ? 'block' : 'none' }} size={26} className='cursor-pointer mb-2' onClick={toggleSidebar} />
-
         <HiMenuAlt3 style={{ display: !open ? 'block' : 'none' }} size={26} className='cursor-pointer mb-2' onClick={toggleSidebar} />
       </div>
       <hr />
       <ul className="list-none p-0">
-        {modules.map((module, index) => (
-          <li key={index} className="mb-5">
-            <a href={module.link} className="flex mt-2 pb-1 hover:bg-gray-900 focus:bg-dark-900 rounded-lg transition duration-300 ease-in-out">
-              <module.icon size={20} className="mt-2 mr-2 text-yellow-500" />
-              <span className={`mt-2 ${open ? 'block' : 'hidden'} transition-opacity duration-300 ease-in-out opacity-${open ? '100' : '0'} ml-${open ? '2' : '0'}`}>{module.name}</span>
-            </a>
-          </li>
-        ))}
+        {/* Manual display of modules */}
+        <li className="mb-5">
+          <a href="/" className="flex mt-2 pb-1 hover:bg-gray-900 focus:bg-dark-900 rounded-lg transition duration-300 ease-in-out">
+            <MdOutlineDashboard size={20} className="mt-2 mr-2 text-yellow-500" />
+            <span className={`mt-2 ${open ? 'block' : 'hidden'} transition-opacity duration-300 ease-in-out opacity-${open ? '100' : '0'} ml-${open ? '2' : '0'}`}>Dashboard</span>
+          </a>
+        </li>
+        <li className="mb-5">
+          <a href="/services" className="flex mt-2 pb-1 hover:bg-gray-900 focus:bg-dark-900 rounded-lg transition duration-300 ease-in-out">
+            <AiOutlineUser size={20} className="mt-2 mr-2 text-yellow-500" />
+            <span className={`mt-2 ${open ? 'block' : 'hidden'} transition-opacity duration-300 ease-in-out opacity-${open ? '100' : '0'} ml-${open ? '2' : '0'}`}>Services</span>
+          </a>
+        </li>
+        {/* Add more manual entries for other modules */}
       </ul>
-
     </div>
   );
 }
