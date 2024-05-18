38bbdb9c70e691b46a1b82676b4cc43044e22ff6
anida15 <anidakimtai@gmail.com>
2024-05-18 15:13:15 +0300

    update

--- src/FooterBar.js
+++ src/FooterBar.js
@@ -16,7 +16,7 @@ const FooterBar = () => {
       <p className="text-sm mb-0">NAIROBI</p>
       <p className="text-sm mb-0">ottomansgs@gmail.com</p>
       <p className="text-sm mb-0">Telephone +254-741457805, +254-775144500</p>
-      <a className="text-sm mb-0" href='https://wa.me/+254790359782' style={{ fontSize: '0.8rem' }}>© 2024 Ottoman. Powered by Snowpiercers.</a>
+      <a className="text-sm mb-0" href='https://github.com/anida15' style={{ fontSize: '0.8rem' }}>© 2024 Ottoman. Powered by Snowpiercers.</a>
 
     </div>
   );
